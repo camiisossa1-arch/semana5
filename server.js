890ac333ed3c84ac93bfdfaf6a5d033d74173cd9
Cami <camiisossa1@gmail.com>
2026-06-02 16:12:38 -0500

    Semana 5 completa - Servidor Node.js y archivos txt

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Servidor activo');
+});
+
+app.get('/saludo', (req, res) => {
+  res.send('Hola comunidad');
+});
+
+app.get('/mensaje/:nombre', (req, res) => {
+  const nombre = req.params.nombre;
+  res.send('Hola ' + nombre);
+});
+
+app.post('/reporte', (req, res) => {
+  const { tipo, descripcion } = req.body;
+  res.send(`Reporte recibido: ${tipo} - ${descripcion}`);
+});
+
+app.listen(3000, () => {
+  console.log('Servidor ejecutándose en puerto 3000');
+});
